eed31f82d02763d4f05429036ab6f942a049d6dc
RyanErik <ryaneriklimadasilva@alu.ufc.br>
2023-06-08 19:01:46 -0300

    primeiro teste

---
+++ teste/src/br/com/ryan/git/hellogit.java
@@ -0,0 +1,10 @@
+package br.com.ryan.git;
+
+public class hellogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+	}
+
+}
---
+++ teste/src/teste/testando.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class testando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
